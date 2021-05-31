a06bed14cf40394877d05a51e8bf62ddf1f1b45a
Antonio Thomacelli <tonnytg@gmail.com>
2021-05-31 09:13:54 -0300

    add GCP SQL

---
+++ GCP SQL/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project     = "lexical-botany-307503"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_database_instance" "instance" {
+  name   = "my-database-instance"
+  region = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gcp-proxyfarm-startup
+
+## Motivação
+
+Criar um Farm de instancias com binário GCP Proxy da Google para receber conexões e redirecionar para o GCP SQL.
+
+## Limitações
+
+GCP SQL aguenta somente 4mil conexões no máximo, mesmo trocando a familia das instâncias.
