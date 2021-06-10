4f757dd81077cf2d002d1ecb4c82eec1990c58cf
Antonio Thomacelli <tonnytg@gmail.com>
2021-06-10 09:19:01 -0300

    demo terraform

--- GCP SQL/main.tf
+++ GCP SQL/main.tf
@@ -16,5 +16,5 @@ resource "google_sql_database_instance" "instance" {
     tier = "db-f1-micro"
   }
 
-  deletion_protection  = "true"
+  deletion_protection  = "false"
 }
---
+++ GCP VPC/main.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project     = "lexical-botany-307503"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
